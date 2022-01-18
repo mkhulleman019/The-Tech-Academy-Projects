e48e6482730d36040b0e441c1e31c2087825117b
mkhulleman019 <mkhulleman019@yahoo.com>
2022-01-18 09:50:13 -0700

    Updated portfolio
    Updated css to be fully responsive (needs minimal changes). Added gallery slideshow.

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -30,9 +30,9 @@ function showSlides(n) {
     if (n > slides.length) {slideIndex =1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].getElementsByClassName.display = "none";
+        slides[i].style.display = "none";
     }
-    for (i =0; i< dots.length; i++) {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace("active", "");
     }
     slides[slideIndex-1].style.display ="block";
